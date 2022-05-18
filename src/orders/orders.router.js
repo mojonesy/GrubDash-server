e26b27b83c9a2915ef87749ec934247ef57b8286
Molly <mojonesycreative@gmail.com>
2022-05-18 15:21:22 -0400

    route for post

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
